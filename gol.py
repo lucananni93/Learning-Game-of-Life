c9aa7c2add35d3c49f8c1b4fa12e4c057674c2e9
lucananni93 <lucananni93@gmail.com>
2018-02-08 00:46:49 +0100

    efficient implementation

--- gol.py
+++ gol.py
@@ -1,51 +1,34 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from scipy.signal import convolve2d
 
 
 ON = 1
 OFF = 0
 INITIAL_DENSITY = 0.3
 
-HEIGHT = 100
-WIDTH = 100
+HEIGHT = 1000
+WIDTH = 1000
+
+conv_kernel = np.array([[1, 1, 1],
+                        [1, 0, 1],
+                        [1, 1, 1]])
 
 
 def init_grid(height, width, init_density):
-    grid = np.random.choice([OFF, ON], size=(height, width), p=[1 - init_density, init_density])
+    grid = np.random.choice([OFF, ON], size=(height, width), p=[
+                            1 - init_density, init_density])
     return grid
 
 
 def update_grid(grid):
-    padded_grid = np.lib.pad(grid, (1,1), 'constant', constant_values=0)
+    count_grid = convolve2d(grid, conv_kernel, "same")
     result_grid = np.zeros_like(grid)
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            patch = padded_grid[i: i + 3, j: j + 3]
-            updated_value = update_value(patch)
-            result_grid[i, j] = updated_value
+
+    result_grid[(count_grid == 2) & (grid == ON)] = ON
+    result_grid[(count_grid == 3)] = ON
     return result_grid
-
-
-def update_value(patch):
-    value = patch[1, 1]
-    n_others = patch.sum() - value
-    if value == ON:
-        if n_others < 2:
-            return OFF      # death by isolation
-        elif 2 <= n_others <= 3:
-            return ON       # survival
-        elif n_others > 3:
-            return OFF      # death by overpopulation
-        else:
-            raise ValueError("n neighbours inconsistent")
-    elif value == OFF:
-        if n_others == 3:
-            return ON       # birth by reproduction
-        else:
-            return OFF
-    else:
-        raise ValueError("Cell is in a unknown state")
 
 
 def main():
@@ -60,7 +43,7 @@ def main():
         grid = updated_grid
         return [mat]
 
-    ani = animation.FuncAnimation(fig, update, interval=50)
+    ani = animation.FuncAnimation(fig, update, interval=50, blit=True)
     plt.show()
 
 
